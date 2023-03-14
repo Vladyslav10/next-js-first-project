4480ce766442b33350b668a26bd654faf1af9090
Vladyslav10 <vshapovalov02@gmail.com>
2023-03-14 12:51:59 +0200

    delete dependencies

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-        port: '',
-        pathname: '/images/M/',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
